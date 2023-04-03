cb1934ebf09693ced5f3464de54bad424884cb2e
arora1703 <abhimanyu.arora@adgonline.in>
2023-04-03 19:44:00 +0530

    interview_task

---
+++ interview_task/database/migrations/2023_04_03_131242_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');            
+            $table->longText('content')->nullable()->default('text');
+            $table->string('image', 100)->nullable()->default('text');
+            $table->integer('likes')->unsigned()->nullable()->default(12);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
